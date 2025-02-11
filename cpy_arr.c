c0ae39b90d2950564fd910ec8c81becc895811c2
Johns Jose <jjkanichukattu@gmail.com>
2025-02-11 16:26:40 +0530

    Add files via upload

---
+++ cpy_arr.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+	int i,n;
+		printf("Enter the size of the array:");
+		scanf("%d",&n); 
+	int arr[n];
+		printf("Enter the elements in your array: ");
+		
+			for(i=0;i<n;i++)
+			{
+			scanf("%d",&arr[i]);
+			}
+	int a[n];
+		printf("Copy of the array: ");
+		
+			for(i=0;i<n;i++)
+			{
+			arr[i]==a[n];
+			printf("%d",a[n]);
+			}
+return 0;
+}
+
